57acfb3940e5b6deb877c606beb9377b2af75f49
slev-v <stasrudometkin2@gmail.com>
2023-09-25 11:11:19 +0300

    kanagawa

--- init.lua
+++ init.lua
@@ -23,7 +23,8 @@ return {
   -- colorscheme = "tokyonight-night",
   -- colorscheme = "tokyonight-moon",
   -- colorscheme = "onedark",
-  colorscheme = "palenight",
+  -- colorscheme = "palenight",
+  colorscheme = "kanagawa",
 
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
--- plugins/community.lua
+++ plugins/community.lua
@@ -6,12 +6,16 @@ return {
 
   { import = "astrocommunity.colorscheme.rose-pine" },
   { import = "astrocommunity.colorscheme.tokyonight-nvim" },
+  { import = "astrocommunity.colorscheme.onedarkpro-nvim" },
+  { import = "astrocommunity.colorscheme.kanagawa-nvim" },
   { import = "astrocommunity.pack.rust" },
   { import = "astrocommunity.motion.nvim-surround" },
-  { import = "astrocommunity.colorscheme.onedarkpro-nvim" },
   { import = "astrocommunity.editing-support.zen-mode-nvim" },
+  { import = "astrocommunity.editing-support.todo-comments-nvim" },
   { import = "astrocommunity.markdown-and-latex.glow-nvim" },
   { import = "astrocommunity.color.transparent-nvim" },
+  { import = "astrocommunity.diagnostics.trouble-nvim" },
+  { import = "astrocommunity.completion.codeium-vim" },
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
 }
