9f2c626061866b92f67b14c353f5ee1b7f19ad04
slev-v <stasrudometkin2@gmail.com>
2023-10-06 19:39:16 +0300

    python pack

--- plugins/community.lua
+++ plugins/community.lua
@@ -9,6 +9,7 @@ return {
   { import = "astrocommunity.colorscheme.onedarkpro-nvim" },
   { import = "astrocommunity.colorscheme.kanagawa-nvim" },
   { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.python" },
   { import = "astrocommunity.motion.nvim-surround" },
   { import = "astrocommunity.editing-support.zen-mode-nvim" },
   { import = "astrocommunity.editing-support.todo-comments-nvim" },
